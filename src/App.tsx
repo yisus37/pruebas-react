d97f487c0f5afd6293eb7b8a76081fca17a66701
yisus37 <jahernandez@exikhan.com.mx>
2023-01-10 13:00:07 -0600

    camara

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [mostrar, setmostrar] = React.useState(false);
-  const camaraRef = React.useRef();
-/*   const camara=React.useRef(new Camara(document.getElementById("player"))); */
-const tomarFoto=()=>{
+  const camaraRef = React.useRef<any>();
+  const streamRef = React.useRef<any>();
+  const [fotoRef,setFotoRef]=React.useState('')
+  const camara=React.useRef(new Camara(document.querySelector('video')));
+
+const onCamara=()=>{
   navigator.mediaDevices.getUserMedia({video: true})
   .then(stream => {
     // Obtén el elemento <video> en tu aplicación de React
-    const videoElement:any = document.querySelector('video');
-
+    camaraRef.current = document.querySelector('video');
     // Asigna el flujo de la cámara al elemento <video>
-    videoElement.srcObject = stream;
+    camaraRef.current.srcObject = stream;
+    streamRef.current=stream
   })
   .catch(error => {
     console.error('Ocurrió un error:', error);
   });
 }
+const offCamara=()=>{
+  tomarFoto()
+  camaraRef.current.pause()
+  if(streamRef.current){
+     streamRef.current.getTracks()[0].stop()
+  }
+}
+
+const tomarFoto=()=>{
+  let canvas:any = document.createElement('canvas')
+  canvas.setAttribute('width',300)
+  canvas.setAttribute('height',300)
+  let context=canvas.getContext('2d')
+  context.drawImage(camaraRef.current,0,0,canvas.width,canvas.height)
+  
+  const foto=context.canvas.toDataURL();
+  canvas=null
+  context=null
+  setFotoRef(foto)
+  console.log(foto)
+
+}
+
 
   
   return (
@@ -26,9 +52,11 @@ const tomarFoto=()=>{
 <h2>Hola Toma Una Foto</h2>
       <div style={{display:mostrar?'flex':'none'}}>
         <video id='player' autoPlay style={{height:'300px'}}></video>
-      <button id='btn' style={{height:50,width:50,marginTop:'auto'}} onClick={()=>tomarFoto()}>Tomar Foto</button>
+      <button id='btn' style={{height:50,width:50,marginTop:'auto'}} onClick={()=>onCamara()}>Abrir</button>
+      <button style={{height:50,width:50,marginTop:'auto'}} onClick={()=>offCamara()}>Tomar Foto</button>
       </div>
       <button id='btn' onClick={()=>setmostrar(!mostrar)}>Foto</button>
+      <img src={fotoRef}></img>
     </div>
   );
 }
@@ -45,14 +73,17 @@ class Camara{
   }
    on(){
 
-    /* navigator.mediaDevices.getUserMedia({
+    navigator.mediaDevices.getUserMedia({
     audio:false,
     video:{width:300,height:300}
-   }) */
-   if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-    console.log("Let's get this party started")
-    navigator.mediaDevices.getUserMedia({video: true})
-  }
+   }).then(s=>{
+
+    console.log(this.videoNode)
+    console.log(this.videoNode.srcObject)
+    this.videoNode.srcObject=s
+    this.stream=s
+   })
+
   }
   off(){
     
